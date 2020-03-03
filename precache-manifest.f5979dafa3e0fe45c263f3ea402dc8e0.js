99d88d04a2106cdc703f130c8a151c23457571b9
Filip Pie <filipiusz@gmail.com>
2020-03-03 13:57:41 +0100

    Updates

---
+++ precache-manifest.f5979dafa3e0fe45c263f3ea402dc8e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1640aaf920b74e2e8ae05cd358463acb",
+    "url": "/tictactoe-app/index.html"
+  },
+  {
+    "revision": "bfc7bc40ca9849acd5d7",
+    "url": "/tictactoe-app/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "9e6b77781aad9505ece4",
+    "url": "/tictactoe-app/static/js/2.b5035cd5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tictactoe-app/static/js/2.b5035cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc7bc40ca9849acd5d7",
+    "url": "/tictactoe-app/static/js/main.f518f4e5.chunk.js"
+  },
+  {
+    "revision": "7ee3016be2b739ed4d88",
+    "url": "/tictactoe-app/static/js/runtime-main.40573eef.js"
+  }
+]);
